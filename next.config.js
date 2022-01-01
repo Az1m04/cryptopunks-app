80c788727ed6a48f84f5bcc1885381e0645c76b0
Azim <azim.babu@simbaquartz.com>
2022-01-01 23:14:44 +0530

    .

--- next.config.js
+++ next.config.js
@@ -2,5 +2,13 @@
 module.exports={
     images:{
         domains:["ipfs.io",]
-    }
+    },
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api.example.com/:path*',
+          },
+        ]
+      },
 }
